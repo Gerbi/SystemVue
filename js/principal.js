0812a9ee18c99fd7be2801e98575dc6b8e79fc64
gerbidev <gerbi@protonmail.com>
2018-08-24 12:38:18 -0700

    i

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,26 @@
-var app = new Vue(){
+var app = new Vue({
     el: '#app',
     data : {
-        mensaje: 'Hola VueJS!'
+        lista: [
+        ], 
+        nombre :'',
+        promedio : ''
+    },
+    methods : {
+        agregarNota: function (){
+            estadoA=false;
+            if(this.promedio>=12.5){
+                estadoA=true;
+            }
+            if(this.nombre!="" && this.promedio!=""){
+                this.lista.push({nombre:this.nombre, promedio:this.promedio, estado:estadoA})
+                this.hombre="";
+                this.promedio=""
+            }
+            else{
+                alert("Ingrese un nombre u promedio del estudiante")
+            }
+        }
+
     }
-}
+})
